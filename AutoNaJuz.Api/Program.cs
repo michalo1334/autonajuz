7c393ef1ee149d0cac951709d677e1e421068e43
Michał Olszewski <miolszewski@outlook.com>
2024-10-20 20:32:12 +0200

    Changes

--- AutoNaJuz.Api/Program.cs
+++ AutoNaJuz.Api/Program.cs
@@ -1,4 +1,5 @@
 using AutoNaJuz.DAL.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
