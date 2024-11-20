4520dd3661a3dc1b612654b9055982d148333992
Michał Olszewski <94755984+michalo1334@users.noreply.github.com>
2024-11-20 21:26:19 +0100

    fhotfix (#26)

--- AutoNaJuz.Api/wwwroot/js/app.js
+++ AutoNaJuz.Api/wwwroot/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5270/api';
+const API_BASE_URL = 'https://localhost:7092/api';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupNavigation();
