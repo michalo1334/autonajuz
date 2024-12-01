5317918aeb76ff24600ab84ecbfc2a60faf95e3a
Michał Olszewski <94755984+michalo1334@users.noreply.github.com>
2024-12-01 17:58:33 +0100

    Added versioning

--- AutoNaJuz.Api/wwwroot/js/app.js
+++ AutoNaJuz.Api/wwwroot/js/app.js
@@ -4,10 +4,19 @@ const API_URL = 'https://localhost:3123/api';
 
 const COMMIT_HASH = '';
 const COMMIT_NAME = '';
+const V_MAJOR = '';
+const V_MINOR = '';
+const V_PATCH = '';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupNavigation();
     loadCars(); // Default view
+
+    let versionElement = document.getElementById('version');
+    versionElement.innerHTML = `v${V_MAJOR}.${V_MINOR}.${V_PATCH}`;
+
+    let commitElement = document.getElementById('commit');
+    commitElement.innerHTML = `Commit: ${COMMIT_HASH} (${COMMIT_NAME})`;
 });
 
 function setupNavigation() {
--- build-and-upload-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build . --no-cache -f ./AutoNaJuz.Web/Dockerfile -t registry.digitalocean.com/auto-na-juz/auto-na-juz-web:debug
-docker build . --no-cache -f ./AutoNaJuz.Api/Dockerfile -t registry.digitalocean.com/auto-na-juz/auto-na-juz-api:debug
-
-docker push registry.digitalocean.com/auto-na-juz/auto-na-juz-api:debug
-docker push registry.digitalocean.com/auto-na-juz/auto-na-juz-web:debug
-
-scp -r -i ~/.ssh/id_ed25519 nginx/ root@67.207.69.161:./app/
-scp -r -i ~/.ssh/id_ed25519 docker-compose.dev/* root@67.207.69.161:./app/
